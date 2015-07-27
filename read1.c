4a68280c128a8d32b2e3811b54202ee54e111ad4
Luca Herlein <luca@herlein.com>
2015-07-27 23:42:32 +0000

    changed to usleep

--- read1.c
+++ read1.c
@@ -8,10 +8,18 @@
 
 int main(void)
 {
+	wiringPiSetup(); // Initializes wiringPi using wiringPi's simlified number system.
+	wiringPiSetupGpio(); // Initializes wiringPi using the Broadcom GPIO pin numbers
+
+	pinMode(4, INPUT);
+
 	char mychar = 0;
 	while(mychar==0)
 	{
-		printf("0\n");
-		sleep(1);
+		if (digitalRead(4))
+    			printf("1\n");
+		else
+    			printf("0\n");
+		usleep(250000);
 	}
 }
